074dd84b7b3d1c5ae4a906bc80d39a934f55e1d0
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2018-08-03 07:41:50 +0000

    Update channel.py

--- LINETCR/api/channel.py
+++ LINETCR/api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/5.5.1.1.590"
-    LA = "DESKTOPWIN\t5.5.1.1.1590\tWINDOWS_NT\t8.0"
+    UA = "Line/8.10.1"
+    LA = "CHROMEOS\t2.1.5\tChrome_OS\t1"
 
     authToken = None
     mid = None
